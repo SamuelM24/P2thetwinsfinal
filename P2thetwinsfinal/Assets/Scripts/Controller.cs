b22eaae858e20fba8ff0a52db5b9378d2f5a67b7
SamuelM24 <49ersownrams@gmail.com>
2023-05-19 16:17:58 -0700

    Revert "saved"
    This reverts commit 24f61a0fad5f61ad8db97c7a406616db47ba14e4.

--- P2thetwinsfinal/Assets/Scripts/Controller.cs
+++ P2thetwinsfinal/Assets/Scripts/Controller.cs
@@ -4,10 +4,6 @@ using UnityEngine;
 
 public class Controller : MonoBehaviour
 {
-    private float speed = 5.0f;
-    private float turnSpeed = 75.0f;
-    private float horizontalInput;
-    private float forwardInput;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,9 +13,6 @@ public class Controller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        horizontalInput = Input.GetAxis("Horizontal");
-        forwardInput = Input.GetAxis("Vertical");
-        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
-        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+        
     }
 }
