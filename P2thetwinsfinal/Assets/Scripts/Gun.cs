114d754cbc4d810d46b329327bcdd7e896f32924
SamuelM24 <49ersownrams@gmail.com>
2023-05-20 00:30:22 -0700

    bullet impact
    bullet impact

--- P2thetwinsfinal/Assets/Scripts/Gun.cs
+++ P2thetwinsfinal/Assets/Scripts/Gun.cs
@@ -1,18 +1,25 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Gun : MonoBehaviour
 {
     public float damage = 10f;
     public float range = 100f;
+    public float fireRate = 15f;
+    public float impactForce = 30f;
 
     public Camera fpsCam;
     public ParticleSystem muzzleFlash;
+    public GameObject impactEffect;
+
+    private float nextTimeToFire = 0f;
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
+        if (Input.GetButtonDown("Fire1") && Time.time >= nextTimeToFire)
         {
+            nextTimeToFire = Time.time + 1f/fireRate;
             Shoot();
         }
     }
@@ -31,6 +38,14 @@ public class Gun : MonoBehaviour
             {
                 target.TakeDamage(damage);
             }
+
+            if (hit.rigidbody != null)
+            {
+                hit.rigidbody.AddForce(-hit.normal * impactForce);
+            }
+
+            GameObject impactGO = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+            Destroy(impactGO, 2f);
         }
     }
 }
