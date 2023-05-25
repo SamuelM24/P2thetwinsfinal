ad674a67f0de2b72edff4a0341760b65928a070e
SamuelM24 <beniciomedina11@gmail.com>
2023-05-25 09:56:26 -0700

    minimap
    minimap

--- P2thetwinsfinal/Assets/Scripts/PlayerMovement.cs
+++ P2thetwinsfinal/Assets/Scripts/PlayerMovement.cs
@@ -17,12 +17,15 @@ public class PlayerMovement : MonoBehaviour
     Vector3 velocity;
     bool isGrounded;
 
+    public bool isSprinting = false;
+    public float sprintingMultiplier;
+
     // Update is called once per frame
     void Update()
     {
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
-        if(isGrounded && velocity.y < 0)
+        if (isGrounded && velocity.y < 0)
         {
             velocity.y = -2f;
         }
@@ -30,11 +33,25 @@ public class PlayerMovement : MonoBehaviour
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
+        if(Input.GetKey(KeyCode.LeftShift))
+        {
+            isSprinting = true;
+        }
+        else
+        {
+            isSprinting = false;
+        }
+
         Vector3 move = transform.right * x + transform.forward * z;
+
+        if (isSprinting == true)
+        {
+            move *= sprintingMultiplier;
+        }
 
         controller.Move(move * speed * Time.deltaTime);
 
-        if(Input.GetButtonDown("Jump") && isGrounded)
+        if (Input.GetButtonDown("Jump") && isGrounded)
         {
             velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
         }
@@ -42,6 +59,5 @@ public class PlayerMovement : MonoBehaviour
         velocity.y += gravity * Time.deltaTime;
 
         controller.Move(velocity * Time.deltaTime);
-
     }
 }
